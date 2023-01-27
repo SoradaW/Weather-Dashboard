dae108a36702689053c4e16c1eeffa5b567175bd
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-01-27 20:35:26 +0000

    Added ajax method to get information from API

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+// store api key from OpenWeatherMap 
+const APIKey = "&0a85def07157d9d6ca1994063d06cb9e";
+
+//store the value of the input
+let currentCity = $("#search-input").val().trim();
+let date = new Date();
+
+$("#search-input").keypress(function(e){
+  if(e.keyCode === 13){
+    e.preventDefault();
+    $("#search-button").click();
+  }
+});
+
+$("#search-button").on("click", function(){
+
+  $("#forecast-heading").addClass("show");
+
+  //get the value from user input
+  currentCity = $("#search-input").val().trim();
+
+  //clear input box
+  //$("#search-input").val("");
+
+//full URL to call API
+const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + APIKey;
+
+$.ajax({
+ url: queryURL,
+ method: "GET"
+}).then(function(response){
+  console.log(response);
+
+  console.log(response.name);
+  console.log(response.weather[0].icon);
+
+  let tempInCelcius = parseFloat(response.main.temp) - 273.15;
+  console.log(Math.floor(tempInCelcius));
+
+  console.log(response.main.humidity);
+  console.log(response.wind.speed);
+
+  currentConditions(response);
+  currentForecast(response);
+  makeList();
+
+  });
+});
