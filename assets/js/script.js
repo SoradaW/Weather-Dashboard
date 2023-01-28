e097dbac51e8edcbd2b6df8a25dbd56ffd197608
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-01-28 16:22:19 +0000

    Added JS code to display currentCity

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 const APIKey = "&0a85def07157d9d6ca1994063d06cb9e";
 
 //store the value of the input
-let city = $("#search-input").val().trim();
+let city = $("#search-input").val();
 let date = new Date();
 let searchHistoryList = [];
 
@@ -21,6 +21,15 @@ function currentConditions(city){
 
     let iconImg = response.weather[0].icon;
     let iconURL = `https://openweathermap.org/img/w/${iconImg}.png`;
+    
+    let currentCity = $(`
+      <h2 id="current-city">
+        ${response.name} ${today} <img src="${iconURL}" alt="${response.weather[0].description}" /> </h2>
+      <p>Temperature: ${response.main.temp} °C</p>
+      <p>Humidity: ${response.main.humidity} %</p>
+      <p>Wind Speed: ${response.wind.speed} MPH</p>
+    `);
 
-  })
+    $("#city-detail").append(currentCity);
+  });
 }
